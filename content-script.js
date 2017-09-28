d4ac07d8e6c550bd122ef0db84768556bdc02b01
philipp <git.99.antijunk@spamgourmet.com>
2017-09-28 17:18:23 +0200

    Initial 0.1

---
+++ content-script.js
@@ -0,0 +1,23 @@
+﻿var openAll = function(e) {
+	e.preventDefault();
+
+	var all = $('.bz_id_column a');
+	var msg = 'You\'re trying to open ' + all.length + ' tabs. ';
+	msg += 'Are you sure you want to do this?';
+	if (all.length < 10 || confirm(msg)) {
+		$('.bz_id_column a').each(function() {
+			if ($(this).attr('href')) {
+				window.open($(this).attr('href'), '_blank');
+			}
+		});
+	}
+};
+
+var a = $('<a>', {'href': '#',
+	'click': openAll,
+	'text': 'Open All in Tabs'}),
+	pipe = $('<span>', {'html': '&nbsp;|'}),
+	td = $('<td>');
+
+td.append(a).append(pipe);
+$('.bz_query_edit').before(td);
