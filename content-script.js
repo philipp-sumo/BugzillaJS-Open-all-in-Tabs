b27aedc922156e6f1f878d623de576a1e39aab3a
philipp <git.99.antijunk@spamgourmet.com>
2018-01-10 18:47:45 +0100

    reverse sorting order due to change in firefox 57

--- content-script.js
+++ content-script.js
@@ -1,11 +1,13 @@
-﻿var openAll = function(e) {
+﻿debugger;
+
+var openAll = function(e) {
 	e.preventDefault();
 
 	var all = $('.bz_id_column a');
 	var msg = 'You\'re trying to open ' + all.length + ' tabs. ';
 	msg += 'Are you sure you want to do this?';
 	if (all.length < 10 || confirm(msg)) {
-		$('.bz_id_column a').each(function() {
+		$($('.bz_id_column a').get().reverse()).each(function() {
 			if ($(this).attr('href')) {
 				window.open($(this).attr('href'), '_blank');
 			}
